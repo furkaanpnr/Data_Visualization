e1861568d45b501fd5ce2cd776a83e3443287f9b
furkaanpnr <furkanpnr220@gmail.com>
2023-01-05 22:27:28 +0300

    Update test.py

--- test.py
+++ test.py
@@ -41,11 +41,46 @@ whiteScores = [data["games"]["game"+str(i+1)]["scores"]["white"] for i in range(
 #plt.show()
 
 ### compare game1 scores with pie chart
-y = np.array(score1)
-plt.title("Game1 Results")
-labels = ["RED", "BLUE", "GREEN", "WHITE"]
-colors = ["red","blue","green","cyan"]
-explode = (0, 0, 0.2, 0)# only "explode" the 3rd slice
-plt.pie(y, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, explode=explode)
-plt.legend(title="AI Players", loc="best", bbox_to_anchor=(0.8, 0.1, 0.5, 1))
+#y = np.array(score1)
+#plt.title("Game1 Results")
+#labels = ["RED", "BLUE", "GREEN", "WHITE"]
+#colors = ["red","blue","green","cyan"]
+#explode = (0, 0, 0.2, 0)# only "explode" the 3rd slice
+#plt.pie(y, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, explode=explode)
+#plt.legend(title="AI Players", loc="best", bbox_to_anchor=(0.8, 0.1, 0.5, 1))
+#plt.show()
+
+### compare RED AI's scores in all games with scatter graph
+#x = [game+1 for game in range(len(data))]
+#y = redScores
+#plt.xlabel("Games")
+#plt.ylabel("RED AI's scores")
+#plt.scatter(x,y)
+#plt.show()
+
+### compare all AI scores
+#>> RED AI 
+x = [game+1 for game in range(len(data))]
+y = redScores
+plt.scatter(x, y, color="red")
+
+#>> BLUE AI
+x = [game+1 for game in range(len(data))]
+y = blueScores
+plt.scatter(x, y, color="blue")
+
+#>> GREEN AI
+x = [game+1 for game in range(len(data))]
+y = greenScores
+plt.scatter(x, y, color="green")
+
+#>> WHITE AI
+x = [game+1 for game in range(len(data))]
+y = whiteScores
+plt.scatter(x, y, color="cyan")
+
+plt.xlabel("GAMES")
+plt.ylabel("AI's SCORES")
+plt.title("RESULT OF ENTİRE GAME")
+#plt.colorbar()
 plt.show()
